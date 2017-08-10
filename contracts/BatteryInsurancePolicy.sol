21983a5564f162633b63ed86c826a170361d4a6e
Darius Devenas <d.devenas@adform.com>
2017-08-10 16:28:20 +0300

    initial revenue commit

--- contracts/BatteryInsurancePolicy.sol
+++ contracts/BatteryInsurancePolicy.sol
@@ -1,28 +1,51 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.14;
 
 import "./PolicyInvestable.sol";
+import "./SafeMath.sol";
 
 contract BatteryInsurancePolicy is PolicyInvestable { 
 
-  // Investment data
+  using SafeMath for uint256;
+  // Investment data 
   mapping (address => uint) public investors;
   uint public totalInvestorsCount;
   uint public totalInvestedAmount;
+  
   uint public totalInsurers;
   uint public totalClaimsPaid;
+  
+  uint128 public investmentsLimit;
+  uint32 public investmentsDeadlineTimeStamp;
+  uint32 public lastPolicyDate;
+  
+  uint8 constant decimalPrecision = 8;
+    
+  mapping (address => DividendLine[]) private payedDividends;
+  uint public payedDividendsAmount;
 
+ 
   // Insurance data
+  uint public policiesLimit;
   mapping (address => PolicyData) insurancePolicies;
   mapping (string => mapping(string => uint) ) insuranceParameters;
   uint public basePremium;
   uint public maxPayout;
   uint loading;
+  uint public writtenPremiumAmount;
+
 
   // Owner is used to confirm policies and claims which came via our server
-  address owner = 0x2033d81c062dE642976300c6eabCbA149e4372BE;
+  address owner = 0xca35b7d915458ef540ade6068dfe2f44e8fa733c;
+
 
   event Insured(string deviceName, uint insurancePrice);
   event Claimed(uint payout); 
+  event DividendsPayed(uint payout); 
+
+  struct DividendLine{
+      uint amount;
+      uint32 transferDate;
+  }
 
   struct PolicyData {
         DeviceData device;
@@ -84,6 +107,11 @@ contract BatteryInsurancePolicy is PolicyInvestable {
 
     // Max payout (0.01 ETH)
     maxPayout = 10000000000000000;
+    
+    investmentsLimit = 1000000000000000000000; //1000 ETH
+    investmentsDeadlineTimeStamp = uint32(now) + 60 days;
+
+    policiesLimit = 10000;
 
     // Loading percentage (expenses, etc)
     loading = 50;
@@ -94,19 +122,9 @@ contract BatteryInsurancePolicy is PolicyInvestable {
     throw;
   }
 
-  function invest() payable returns (bool success) {
-    if (msg.value == 0) {
-      throw;
-    }
+  
 
-    investors[msg.sender] = investors[msg.sender] + msg.value;
-    totalInvestorsCount++;
-    totalInvestedAmount = totalInvestedAmount + msg.value;
-    Invested(msg.value);
-    return true;
-  }
-
-
+  // policy part
   // More parameters should be included
   function policyPrice(string deviceBrand, string deviceYear, string wearLevel, string region) constant returns(uint price) {
     // set defaults
@@ -136,28 +154,29 @@ contract BatteryInsurancePolicy is PolicyInvestable {
     return officePremium;
   }
 
-
   function insure(string itemId, string deviceBrand, string deviceYear, string wearLevel, string region) payable returns (bool insured) {
+    require(totalInsurers < policiesLimit);
+
     uint totalPrice = policyPrice(deviceBrand, deviceYear, wearLevel, region);
     uint monthlyPayment = totalPrice / 12;
-    if (msg.value < monthlyPayment) {
-      throw;
-    }
+    
+    writtenPremiumAmount += totalPrice; 
+
+    require(msg.value >= monthlyPayment);
 
     var deviceData = DeviceData(itemId, deviceBrand, deviceYear, wearLevel);
     var policy = PolicyData(deviceData, now + 1 years, now + 30 days, monthlyPayment, maxPayout, totalPrice, region, false, false);
 
     insurancePolicies[msg.sender] = policy;
     totalInsurers = totalInsurers + 1;
+    lastPolicyDate = policy.endDateTimestamp;
 
     Insured(deviceBrand, msg.value);
     return true;
   }
 
   function confirmPolicy(address policyOwner) {
-    if(owner != msg.sender) {
-      throw;
-    }
+    require(owner == msg.sender);
 
     insurancePolicies[policyOwner].confirmed = true;
   }
@@ -195,8 +214,106 @@ contract BatteryInsurancePolicy is PolicyInvestable {
     return insurancePolicies[msg.sender].claimed;
   }
 
+ 
+ //investor Part
+  function invest() payable returns (bool success) {
+      require(msg.value > 0);
+  
+      investors[msg.sender] = investors[msg.sender] + msg.value;
+      totalInvestorsCount++;
+      totalInvestedAmount = totalInvestedAmount + msg.value;
+      Invested(msg.value);
+      return true;
+  }
 
+  function isInvestmentPeriodEnded() constant returns (bool) {
+    return (investmentsDeadlineTimeStamp < now);
+  }
 
+  function checkAvailableDividends() constant returns (uint) {       
+    uint dividends = calculateDividends();
 
+    return dividends;
+  }
+
+  function transferDividends() returns (bool){
+    uint dividends = checkAvailableDividends();
+
+    if(dividends > 0)
+    {
+      var dividendLine = DividendLine(dividends, uint32(now));
+
+      payedDividends[msg.sender].push(dividendLine);
+      payedDividendsAmount += dividends;
+      DividendsPayed(dividends);
+    }   
+  }
+
+  function getFreeBalance() private constant returns (int) {
+    return int(writtenPremiumAmount - totalClaimsPaid);
+  }
+
+ 
+ function getInvestorProportion() private constant returns (uint) {
+    //temproray fast calculations TODO: use model calculations
+    uint investedAmount = investors[msg.sender];
+    
+    if (investedAmount > 0) 
+    {
+      uint proportion = investedAmount.mul(100).mul(uint(10)**decimalPrecision).div(totalInvestedAmount); 
+      return proportion;
+    }
+    
+    return 0;
+  }
+
+  // return weis
+  function calculateDividends() private returns (uint) {
+     // check user invested
+    uint investorProportion = getInvestorProportion();
+    
+    if (investorProportion > 0)
+    {  
+      int insurePackageBalance = getFreeBalance();     
+      //if all policies ended
+      if(now > lastPolicyDate)
+      {     
+        int totalFreeBalance = insurePackageBalance + int(totalInvestedAmount);
+        if (insurePackageBalance > 0)
+        {           
+            uint investorPart = uint(insurePackageBalance).mul(uint(10)**decimalPrecision).mul(investorProportion).div(uint(100).mul(uint(10)**decimalPrecision));
+            
+            return investorPart.mul(uint(10)**decimalPrecision);
+        }            
+      }
+      //policies are not ended. Return only 0.01 part from user investment as an example;
+      else
+      { 
+        uint investedAmount = investors[msg.sender];
+        uint8 dividendsLines = payedDividends[msg.sender].length;
+      
+        uint availableDividend = investedAmount.div(100);
+        //return 1 percent of investition on first time
+        if(dividendsLines == 0)
+        {
+          return availableDividend; 
+        } 
+        else
+        {
+          var lastDividendDate = payedDividends[msg.sender][dividendsLines-1].transferDate;
+
+          uint32 dateDiff = now.sub(lastDividendDate);
+          uint32 allowedDifference = 5 * 24 * 60 * 60; // allow dividends each 5 dates;
+
+          if(dateDiff > allowedDifference)
+          {
+            return availableDividend;
+          } 
+        }        
+      }
+    }
+    
+    return 0;    
+  }
 
 }
--- contracts/PolicyInvestable.sol
+++ contracts/PolicyInvestable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.10;
+pragma solidity ^0.4.14;
 
 contract PolicyInvestable {
   function invest() payable returns (bool success);
